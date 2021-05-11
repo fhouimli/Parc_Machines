ddd3091ff2bb80148838e873ed2090dbb6ea30d6
MHO <med.houimli@outlook.fr>
2021-05-11 17:15:06 +0200

    commit parcMachines.py

--- parcMachines.py
+++ parcMachines.py
@@ -1,9 +1,9 @@
-def listerMachines(nomFichier):
+def listAllMachines(nomFichier):
     fichier = open(nomFichier, "r")
     print(fichier.read())
     fichier.close()
 
-#listerMachines("listMachines.txt")
+#listAllMachines("listMachines.txt")
 
 class Machines:
     def __init__(self, nom, ip, cpu, ram, ddr, os):
@@ -13,10 +13,8 @@ class Machines:
         self.ram = ram
         self.ddr = ddr
         self.os = os
-
-#print(machine1.ip)
     
-def addMachines(nomFichier):
+def addMachine(nomFichier):
     # saisir les infos machine
     nom1 = str(input("entrez le nom de la machine: "))
     ip1 = str(input("entrez l'adresse ip: "))
@@ -28,9 +26,18 @@ def addMachines(nomFichier):
     # construire l'instance machine            
     machine1 = Machines(nom1, ip1, cpu1, ram1, ddr1, os1)
     # écriture dans un fichier
-    #nomFichier= 'listMachines.txt'
     fichier = open(nomFichier, "a")
-    fichier.writelines('\n'+machine1.nom+", "+machine1.ip+", "+machine1.cpu+", "+machine1.ram+", "+machine1.ddr+", "+machine1.os)
+    fichier.writelines(machine1.nom+", "+machine1.ip+", "+machine1.cpu+", "+machine1.ram+", "+machine1.ddr+", "+machine1.os+'\n')
     fichier.close()
     
-addMachines("listMachines.txt")
+#addMachine("listMachines.txt")
+
+def listMachineByHostname(hostname, nomFichier):
+
+    fichier = open(nomFichier,"r")
+    for ligne in fichier:
+        if hostname in ligne:
+            print(ligne)
+    fichier.close()
+
+listMachineByHostname("nexus", "listMachines.txt")
