c06211263da423d9941bbf8345a264817a41e8fb
MHO <med.houimli@outlook.fr>
2021-05-12 18:15:21 +0200

    script ajout de delete et modify machine

--- parcMachines.py
+++ parcMachines.py
@@ -1,9 +1,11 @@
+import csv
+
+
 def listAllMachines(nomFichier):
     fichier = open(nomFichier, "r")
     print(fichier.read())
     fichier.close()
 
-#listAllMachines("listMachines2.txt")
 #listAllMachines("listMachines.txt")
 
 class Machines:
@@ -28,10 +30,22 @@ def addMachine(nomFichier):
     machine1 = Machines(nom1, ip1, cpu1, ram1, ddr1, os1)
     # écriture dans un fichier
     fichier = open(nomFichier, "a")
-    fichier.writelines(machine1.nom+", "+machine1.ip+", "+machine1.cpu+", "+machine1.ram+", "+machine1.ddr+", "+machine1.os+'\n')
+    fichier.writelines('\n'+machine1.nom+", "+machine1.ip+", "+machine1.cpu+", "+machine1.ram+", "+machine1.ddr+", "+machine1.os)
     fichier.close()
     
 #addMachine("listMachines2.txt")
+
+def find(hostname, nomFichier):
+    with open(nomFichier, 'rt') as fichier:
+         reader = csv.reader(fichier, delimiter=',')
+         line_count = -1
+         line_found = -1
+         for line in reader:
+                line_count +=1
+                if hostname == line[0]: 
+                  line_found = line_count
+    print ('le hostname existe dans la ligne N° '+str(line_found))
+    return line_found
 
 
 def listMachineByHostname(nomFichier):
@@ -45,7 +59,34 @@ def listMachineByHostname(nomFichier):
     if isHostname == False:
         print("le hostname saisi n'existe pas")
     fichier.close()
+    
 #listMachineByHostname("listMachines.txt")
 
+def deleteMachine(hostname, nomFichier):
+    fichier = open(nomFichier, "r")
+    lines = fichier.readlines()
+    fichier.close()
+    #supprimer ligne
+    position = find(hostname, nomFichier)
+    if position != -1:
+        del lines[position]
+    #réecrir fichier apres suppression ligne
+    fichier = open(nomFichier, "w+")
+    for line in lines:
+        fichier.write(line)
+    fichier.close()
+    return position
 
-
+def deleteMachineByHostname(nomFichier):
+    hostname = input("entrez le nom de la machine à supprimer: ")
+    deleteMachine(hostname, nomFichier)   
+  
+    
+def modifMachineByHostname(nomFichier):
+    hostname = input("entrez le nom de la machine à modifier: ")
+    position = deleteMachine(hostname, nomFichier) 
+    if position != -1:
+        print("entrez les nouvelles informations")
+        addMachine(nomFichier)
+            
+modifMachineByHostname("listMachines.txt")
